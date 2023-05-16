4fb285614f7297b42dc1767676d262b14a24846f
Ubuntu <ubuntu@ip-10-0-12-101.ec2.internal>
2023-05-16 16:39:56 +0000

    commit

--- workshops/otel/microservices-demo/deploy-good.sh
+++ workshops/otel/microservices-demo/deploy-good.sh
@@ -1,3 +1,3 @@
 export PYTHON_TEST_URLGOOD=GOOD
-# envsubst < deploy.yaml  | kubectl apply -f -
-kubectl apply -f deploy.yaml
+envsubst < deploy.yaml  | kubectl apply -f -
+# kubectl apply -f deploy.yaml

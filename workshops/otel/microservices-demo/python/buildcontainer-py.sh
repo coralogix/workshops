6a7a2988499a049770baeadd2d027ef3827e402f
Ubuntu <ubuntu@ip-10-0-12-101.ec2.internal>
2023-06-22 03:16:20 +0000

    commit

--- workshops/otel/microservices-demo/python/buildcontainer-py.sh
+++ workshops/otel/microservices-demo/python/buildcontainer-py.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
 sudo docker build . -f dockerfile-microsvcsdemo -t microsvcsdemo-python && \
-sudo docker tag microsvcsdemo-python public.ecr.aws/w3s4j9x9/microsvcsdemo-python:latest
-sudo docker push public.ecr.aws/w3s4j9x9/microsvcsdemo-python:latest
+sudo docker tag microsvcsdemo-python public.ecr.aws/w3s4j9x9/microservices-demo:python
+sudo docker push public.ecr.aws/w3s4j9x9/microservices-demo:python

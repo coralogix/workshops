27b60c703a3959a6667b5a1e5cddf051ea606c9d
slernercx <steve.lerner@coralogix.com>
2023-06-21 22:59:49 -0400

    commit

--- workshops/otel/microservices-demo/python/buildcontainer-py.sh
+++ workshops/otel/microservices-demo/python/buildcontainer-py.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
 sudo docker build . -f dockerfile-microsvcsdemo -t microsvcsdemo-python && \
-sudo docker tag microsvcsdemo-python stevelerner/microsvcsdemo-python
-sudo docker push stevelerner/microsvcsdemo-python
+sudo docker tag microsvcsdemo-python public.ecr.aws/w3s4j9x9/microsvcsdemo-python:latest
+sudo docker push public.ecr.aws/w3s4j9x9/microsvcsdemo-python:latest
--- workshops/otel/microservices-demo/python/buildcontainer-redis.sh
+++ workshops/otel/microservices-demo/python/buildcontainer-redis.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
 sudo docker build . -f dockerfile-redis -t redis && \
-sudo docker tag redis stevelerner/redis && \
-sudo docker push stevelerner/redis
+sudo docker tag redis public.ecr.aws/w3s4j9x9/redis:latest && \
+sudo docker push public.ecr.aws/w3s4j9x9/redis:latest

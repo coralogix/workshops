e8f02f0d09b54d82d2f1fdd85f575744684df41a
Ubuntu <ubuntu@ip-10-0-11-205.ec2.internal>
2023-07-04 19:05:09 +0000

    commit

--- workshops/otel/microservices-demo/delete-all.sh
+++ workshops/otel/microservices-demo/delete-all.sh
@@ -3,3 +3,5 @@ kubectl delete service cx-payment-gateway-flask
 kubectl delete deployment cx-redis
 kubectl delete service cx-redis
 kubectl delete deployment cx-shopping-cart-reqs
+kubectl delete deployment cx-inventory-java-reqs
+kubectl delete deployment cx-marketing-node-reqs

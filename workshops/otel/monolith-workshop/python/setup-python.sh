a6370c5d9d83db72896da2f845b5410e7577dca6
Ubuntu <ubuntu@ip-172-32-13-14.us-east-2.compute.internal>
2023-12-18 21:21:40 +0000

    commit

--- workshops/otel/monolith-workshop/python/setup-python.sh
+++ workshops/otel/monolith-workshop/python/setup-python.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 pip3 install requests
-pip3 install opentelemetry-distro 
-pip3 install opentelemetry-exporter-otlp 
-pip3 install opentelemetry-instrumentation-requests
-~/.local/bin/opentelemetry-bootstrap
+pip3 install opentelemetry-distro==0.41b0 opentelemetry-exporter-otlp==1.20.0
+~/.local/bin/opentelemetry-bootstrap -a install

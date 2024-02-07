aa3f717415f999b9535c63b118a6fd1af292ecdb
slernercx <130906207+slernercx@users.noreply.github.com>
2024-02-07 15:26:36 -0500

    Update setup-python-env.sh

--- workshops/otel/monolith-workshop/python/setup-python-env.sh
+++ workshops/otel/monolith-workshop/python/setup-python-env.sh
@@ -1,6 +1,9 @@
-export OTEL_RESOURCE_ATTRIBUTES=service.name=service-python,application.name=app-python,api.name=subsystem-python,cx.application.name=app-python,cx.subsystem.name=subsystem-python
+export OTEL_RESOURCE_ATTRIBUTES=service.name=service-python,cx.application.name=app-python,cx.subsystem.name=subsystem-python
 export OTEL_TRACES_EXPORTER=otlp
 export OTEL_METRICS_EXPORTER=otlp
 export OTEL_LOGS_EXPORTER=otlp
-export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true
+
 export OTEL_PYTHON_LOG_CORRELATION=true
+# export OTEL_PYTHON_LOG_FORMAT="%(msg)s [json.trace_id=%(span_id)s]"
+export OTEL_PYTHON_LOG_LEVEL=DEBUG
+export OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true

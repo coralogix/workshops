3f16ba60e569ec0bbaa734f8420b144e85a0918e
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-06-28 15:14:07 +0000

    commit

--- workshops/otel/autogenerators/java/monolith/5-monolith-setup-otel-env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Set environment variables
-export OTEL_SERVICE_NAME=cx-java-autogen
-export OTEL_RESOURCE_ATTRIBUTES="cx.application.name=cx-java-autogen,cx.subsystem.name=cx-java-autogen"
-
-# Print to verify
-echo "OTEL_SERVICE_NAME is set to $OTEL_SERVICE_NAME"
-echo "OTEL_RESOURCE_ATTRIBUTES is set to $OTEL_RESOURCE_ATTRIBUTES"

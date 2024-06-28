f0ebb3c1e6e45a5e0a4eecd944c6c34c2ce170e7
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-06-28 16:18:25 +0000

    commit

--- workshops/otel/autogenerators/java/monolith/5-monolith-run-app.sh
+++ workshops/otel/autogenerators/java/monolith/5-monolith-run-app.sh
@@ -2,5 +2,5 @@ export OTEL_SERVICE_NAME=cx-java-autogen
 export OTEL_RESOURCE_ATTRIBUTES="cx.application.name=cx-java-autogen,cx.subsystem.name=cx-java-autogen"
 
 java -javaagent:/opt/opentelemetry-javaagent.jar \
--Dlog4j.configurationFile=src/main/resources/log4j2.xml \
+-Dlog4j.configurationFile=classpath:log4j2.xml \
 -jar target/java-app-1.0-SNAPSHOT.jar

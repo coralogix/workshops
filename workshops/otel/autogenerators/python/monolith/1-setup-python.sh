80a953425f83449679fd233a4065025454724da9
Steve Lerner <sl@Steves-Laptop.local>
2024-08-13 14:35:06 -0400

    commit

--- workshops/otel/autogenerators/python/monolith/1-setup-python.sh
+++ workshops/otel/autogenerators/python/monolith/1-setup-python.sh
@@ -2,5 +2,6 @@ sudo apt install -y python3.12-venv
 python3 -m venv env
 source env/bin/activate
 pip3 install requests Flask
-pip3 install opentelemetry-distro opentelemetry-exporter-otlp
+# pip3 install opentelemetry-distro opentelemetry-exporter-otlp
+pip3 install opentelemetry-distro==0.46b0 opentelemetry-exporter-otlp==1.25.0
 opentelemetry-bootstrap -a install

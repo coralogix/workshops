dd170662049d1cee6e1bc2cc504be02cc63dfb33
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-08-20 21:58:47 +0000

    commit

--- workshops/otel/autoinject/dotnet8-linux/container/buildcontainer.sh
+++ workshops/otel/autoinject/dotnet8-linux/container/buildcontainer.sh
@@ -1,6 +1,6 @@
 dotnet clean
 dotnet build
-sudo docker build . -t microsvcsdemo-dotnet8
-sudo docker tag microsvcsdemo-dotnet8 public.ecr.aws/w3s4j9x9/autoinject-demo:dotnet8
+sudo docker build . -t autoinject-dotnet8
+sudo docker tag autoinject-dotnet8 public.ecr.aws/w3s4j9x9/autoinject-demo:dotnet8
 sudo docker push public.ecr.aws/w3s4j9x9/autoinject-demo:dotnet8
 dotnet clean

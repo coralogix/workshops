ca5fae6f25d47f257f89c7c130a386681bc53f0c
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-09-09 16:13:36 +0000

    commit

--- workshops/otel/autoinject/java/entrypoint.sh
+++ workshops/otel/autoinject/java/entrypoint.sh
@@ -1,5 +1,7 @@
-# change the endoint env variable in the deployment .yaml to change location of your agent
-# change target URL below to alter the test target
 java \
 -Dlog4j.configurationFile=classpath:log4j2.xml \
+-Dcom.sun.management.jmxremote \
+-Dcom.sun.management.jmxremote.port=9999 \
+-Dcom.sun.management.jmxremote.authenticate=false \
+-Dcom.sun.management.jmxremote.ssl=false \
 -jar java-app-1.0-SNAPSHOT.jar

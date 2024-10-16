e886965138a487caa2a78b5ba55b0ee4e769f5f5
Steve Lerner <sl@Steves-Laptop.local>
2024-10-16 14:53:20 -0400

    commit

--- workshops/rum/1-setup-node.sh
+++ workshops/rum/1-setup-node.sh
@@ -1,6 +1,6 @@
 npm init -y
 npm install @coralogix/browser
-npm install -g @coralogix/rum-cli
+sudo npm install -g @coralogix/rum-cli
 npm install express
 npm install webpack webpack-cli --save-dev
 npm install --save-dev html-webpack-plugin

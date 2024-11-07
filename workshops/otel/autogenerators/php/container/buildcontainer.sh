7e51855115e9ddd4a368e29a9aa380cbaa88a1a6
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-11-07 03:06:21 +0000

    commit

--- workshops/otel/autogenerators/php/container/buildcontainer.sh
+++ workshops/otel/autogenerators/php/container/buildcontainer.sh
@@ -1,4 +1,4 @@
 # requires dockerhub login
-sudo docker build . -t php-augoten
+sudo docker build . -t php-autogen
 sudo docker tag php-augoten public.ecr.aws/w3s4j9x9/microservices-demo:php-autogen
 sudo docker push public.ecr.aws/w3s4j9x9/microservices-demo:php-autogen

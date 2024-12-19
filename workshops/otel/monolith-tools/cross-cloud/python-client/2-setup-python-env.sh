2fd1edd6986b6d80eef6b3a900da707dd7c2ef3a
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2024-12-19 16:56:14 +0000

    commit

--- workshops/otel/monolith-tools/cross-cloud/python-client/2-setup-python-env.sh
+++ workshops/otel/monolith-tools/cross-cloud/python-client/2-setup-python-env.sh
@@ -4,8 +4,7 @@ unset OTEL_TRACES_EXPORTER
 unset OTEL_NODE_RESOURCE_DETECTORS
 unset OTEL_EXPORTER_OTLP_ENDPOINT
 unset OTEL_SERVICE_NAME
-export OTEL_RESOURCE_ATTRIBUTES=service.name=cx-python-client,cx.application.name=cx-python-client,cx.subsystem.name=cx-pytho
-n-client
+export OTEL_RESOURCE_ATTRIBUTES=service.name=cx-python-client,cx.application.name=cx-python-client,cx.subsystem.name=cx-python-client
 sudo rm -rf /var/log/cx
 sudo mkdir -p /var/log/cx
 sudo chown steve_lerner:steve_lerner /var/log/cx

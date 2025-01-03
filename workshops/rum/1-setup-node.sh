2050248eb8c4d213540f5f0c965cb5a9b6f54c52
slernercx <steve.lerner@coralogix.com>
2025-01-02 21:15:10 -0500

    commit

--- workshops/rum/1-setup-node.sh
+++ workshops/rum/1-setup-node.sh
@@ -1,5 +1,6 @@
+npm cache clean
 npm init -y
-npm install @coralogix/browser
-sudo npm install -g @coralogix/rum-cli
+npm i @coralogix/browser@1.4.6 --force
+# npm i @coralogix/browser@latest
 npm install express
 npm install --save-dev webpack webpack-cli html-webpack-plugin
--- workshops/rum/3-sourcemap.sh
+++ workshops/rum/3-sourcemap.sh
@@ -1,5 +1,6 @@
+sudo npm install -g @coralogix/rum-cli --force
 coralogix-rum-cli upload-source-maps -k "cxtp_TTmCDmYLv6kMuRSMXo2el1UxcYoo5l" \
                   -a "steve.lerner@coralogix.com" \
                   -e "US2" \
                   -v "v1" \
-                  -f "<folder location, e.g. ./dist/test>"
+                  -f "./dist"

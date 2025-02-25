c98ab6061965f509ab7deb911334e25d5d1ce300
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2025-02-25 02:48:24 +0000

    commit

--- workshops/otel/autogenerators/java/container/entrypoint.sh
+++ workshops/otel/autogenerators/java/container/entrypoint.sh
@@ -1,9 +1,9 @@
-java -javaagent:/opt/opentelemetry-javaagent.jar \
+java \
+-javaagent:/opt/opentelemetry-javaagent.jar \
+-javaagent:/opt/jmx_prometheus_javaagent-1.1.0.jar=9090:jmx_exporter_config.yaml \
 -Dlog4j.configurationFile=classpath:log4j2.xml \
 -Dcom.sun.management.jmxremote \
 -Dcom.sun.management.jmxremote.port=9999 \
--Dcom.sun.management.jmxremote.authenticate=true \
+-Dcom.sun.management.jmxremote.authenticate=false \
 -Dcom.sun.management.jmxremote.ssl=false \
--Dcom.sun.management.jmxremote.access.file=/etc/jmxremote.access \
--Dcom.sun.management.jmxremote.password.file=/etc/jmxremote.password \
 -jar java-app-1.0-SNAPSHOT.jar
---
+++ workshops/otel/autogenerators/java/storage/jmx-metrics/deploy-jmx-gatherer.sh
@@ -0,0 +1,1 @@
+kubectl apply -f jmx-gatherer-deployment.yaml

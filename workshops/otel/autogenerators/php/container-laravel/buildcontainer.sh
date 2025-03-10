2055242a00a0c4995b14c23de0b1b8d3909df20f
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2025-03-10 17:10:02 +0000

    commit

--- workshops/otel/autogenerators/php/container-laravel/buildcontainer.sh
+++
@@ -1,4 +0,0 @@
-# requires dockerhub login
-sudo docker build . -t php-laravel
-sudo docker tag php-laravel public.ecr.aws/w3s4j9x9/microservices-demo:php-laravel
-sudo docker push public.ecr.aws/w3s4j9x9/microservices-demo:php-laravel

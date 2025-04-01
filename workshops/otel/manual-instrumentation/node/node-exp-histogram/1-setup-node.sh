928a9fc4ae19acbebb53751aea296fc6bad1cd22
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2025-04-01 17:35:26 +0000

    commit

--- workshops/otel/manual-instrumentation/node/node-exp-histogram/1-setup-node.sh
+++ workshops/otel/manual-instrumentation/node/node-exp-histogram/1-setup-node.sh
@@ -1,7 +1,7 @@
 npm install \
-  @opentelemetry/api \
-  @opentelemetry/sdk-metrics \
-  @opentelemetry/exporter-metrics-otlp-grpc \
-  @opentelemetry/resources \
-  @opentelemetry/semantic-conventions \
-  @grpc/grpc-js
+    @opentelemetry/api \
+    @opentelemetry/sdk-metrics \
+    @opentelemetry/exporter-metrics-otlp-grpc \
+    @opentelemetry/resources \
+    @opentelemetry/semantic-conventions \
+    @grpc/grpc-js

39fcf468ab7c8896bb6658c33c97bae3df302b01
Ubuntu <ubuntu@ip-192-168-66-104.us-west-2.compute.internal>
2025-04-08 17:18:59 +0000

    commit

---
+++ workshops/otel/sampling/04-deploy-tailsampling-app2-10percent.sh
@@ -0,0 +1,1 @@
+helm upgrade otel-coralogix-integration coralogix/otel-integration --values tailsampling-app2-10.yaml
---
+++ workshops/otel/sampling/delete-autogen-py.sh
@@ -0,0 +1,4 @@
+kubectl delete deployment cx-py-1
+kubectl delete service cx-py-1
+kubectl delete deployment cx-py-2
+kubectl delete service cx-py-2

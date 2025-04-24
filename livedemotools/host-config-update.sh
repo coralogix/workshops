f7d304ff69aeabedc22b3ece6cd3cd3ef0d5c6f9
Ubuntu <ubuntu@ip-192-168-93-3.us-west-2.compute.internal>
2025-04-24 01:42:01 +0000

    commit

---
+++ livedemotools/host-config-update.sh
@@ -0,0 +1,3 @@
+sudo cp ~/config.demo.yaml /etc/otelcol-contrib/config.yaml
+sudo systemctl restart otelcol-contrib.service
+sudo systemctl status otelcol-contrib.service
--- livedemotools/host-install-otel.sh
+++ livedemotools/host-install-otel.sh
@@ -1,4 +1,6 @@
 sudo dpkg -i [your downloaded version of otel here]
 sudo cp ~/config.demo.yaml /etc/otelcol-contrib/config.yaml
+# copy the config file to the home directory for easy access
+sudo cp ~/config.demo.yaml ~/config.yaml
 sudo systemctl restart otelcol-contrib.service
 sudo systemctl status otelcol-contrib.service

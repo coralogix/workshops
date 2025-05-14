5ea5dfed8ec7292bcd36db05682a1168c3f60a41
Ubuntu <ubuntu@ip-192-168-93-3.us-west-2.compute.internal>
2025-05-14 01:30:51 +0000

    commit

--- workshops/otel/autogenerators/dotnet8mssql/dotnetdb/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ workshops/otel/autogenerators/dotnet8mssql/dotnetdb/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f074cc59f25880fa3771b40fa2e717affe6ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90209a8114f5bda3af2beaec5997191af9185e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
